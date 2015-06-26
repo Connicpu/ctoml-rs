2a623d92ca72eb99b7e7d8e3f806aafbc2b59576
Connor Hilarides <connorcpu@live.com>
2015-06-26 04:20:55 -0700

    Very useful

---
+++ include/ctoml.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+#define CTOML_API extern "C" __declspec(dllimport)
+
+namespace ctoml
+{
+    struct TomlValue;
+    struct TomlTable;
+    struct TomlArray;
+
+    enum class TomlType
+    {
+        String,
+        Integer,
+        Float,
+        Boolean,
+        Datetime,
+        Array,
+        Table,
+    };
+
+    struct Buffer
+    {
+        char *data;
+        size_t len;
+    };
+
+    struct ConstBuffer
+    {
+        ConstBuffer()
+            : data(nullptr), len(0)
+        {
+        }
+
+        ConstBuffer(const char *data, size_t len)
+            : data(data), len(len)
+        {
+        }
+
+        ConstBuffer(const std::string &str)
+            : data(str.data()), len(str.size())
+        {
+        }
+
+        const char *data;
+        size_t len;
+    };
+
+    struct ConstBufferList
+    {
+        ConstBuffer *data;
+        size_t len;
+    };
+
+    ////////////////////////////////////////////////////////////////////
+    // Value functions
+
+    // Get the type of a value
+    CTOML_API TomlType ValueType(const TomlValue *value);
+
+    // Free a TomlValue that you own
+    CTOML_API void FreeValue(TomlValue *value);
+
+    // Create a new table, you own it.
+    CTOML_API TomlValue *CreateTable();
+    // Retrieve the table from a value
+    CTOML_API const TomlTable *ValueTable(const TomlValue *value);
+    CTOML_API TomlTable *ValueTableMut(TomlValue *value);
+
+    // Create a new array, you own it.
+    CTOML_API TomlValue *CreateArray();
+    // Retrieve the table from a value
+    CTOML_API const TomlArray *ValueArray(const TomlValue *value);
+    CTOML_API TomlArray *ValueArrayMut(TomlValue *value);
+
+    // Create a new string, you own it.
+    CTOML_API TomlValue *CreateString(ConstBuffer input);
+    // Retrieve the string value
+    CTOML_API ConstBuffer ValueString(const TomlValue *value);
+
+    // Create a new integer, you own it.
+    CTOML_API TomlValue *CreateInteger(int64_t i);
+    // Retrieve the integer value
+    CTOML_API int64_t ValueInteger(const TomlValue *value);
+
+    // Create a new float, you own it
+    CTOML_API TomlValue *CreateFloat(double f);
+    // Retrieve the float value
+    CTOML_API double ValueFloat(const TomlValue *value);
+
+    // Create a new boolean, you own it
+    CTOML_API TomlValue *CreateBoolean(bool b);
+    // Retrieve the bool value
+    CTOML_API bool ValueBoolean(const TomlValue *value);
+
+    // Create a new Datetime, you own it
+    CTOML_API TomlValue *CreateDatetime(ConstBuffer input);
+    // Retrieve the date string
+    CTOML_API ConstBuffer ValueDatetime(const TomlValue *value);
+
+    ////////////////////////////////////////////////////////////////////
+    // Table functions
+
+    // Input should be UTF-8 encoded data. You own the table.
+    CTOML_API TomlTable *ParseTable(ConstBuffer input);
+
+    // Serializes to a string, get the result with ValueString, you are responsible for calling FreeValue
+    CTOML_API TomlValue *SerializeTable(const TomlTable *table);
+
+    // Free a table that you own
+    CTOML_API void FreeTable(TomlTable *table);
+
+    // Returns the number of keys in the table
+    CTOML_API uint64_t TableKeyCount(const TomlTable *table);
+
+    // Get a list of all of the keys in the table, make
+    // sure you call TableKeyCount and allocate enough ConstBuffers for it.
+    // Modifying the table invalidates the result, but it does not need to be freed
+    CTOML_API void TableKeys(const TomlTable *table, ConstBufferList keys);
+
+    // Get a value from the table with the given key. The table maintains
+    // ownership of the value.
+    CTOML_API const TomlValue *TableGet(const TomlTable *table, ConstBuffer key);
+    CTOML_API TomlValue *TableGetMut(TomlTable *table, ConstBuffer key);
+
+    // Add a value to the table at the given key. You must have ownership of value,
+    // and this function transfers ownership to the table.
+    CTOML_API void TableSet(TomlTable *table, ConstBuffer key, TomlValue *value);
+
+    // Remove the key from the table
+    CTOML_API void TableRemove(TomlTable *table, ConstBuffer key);
+
+    ////////////////////////////////////////////////////////////////////
+    // Array functions
+
+    CTOML_API size_t ArrayCount(const TomlArray *array);
+    CTOML_API const TomlValue *ArrayGet(const TomlArray *array, size_t index);
+    CTOML_API TomlValue *ArrayGetMut(TomlArray *array, size_t index);
+    CTOML_API void ArrayPush(TomlArray *toml, TomlValue *value);
+    CTOML_API void ArrayPop(TomlArray *toml);
+}
---
+++ src/lib.rs
@@ -0,0 +1,231 @@
+#![allow(non_snake_case)]
+#![feature(box_syntax)]
+
+extern crate toml;
+
+use std::mem;
+
+pub type TomlValue = toml::Value;
+
+#[repr(C)]
+pub enum TomlType {
+    String,
+    Integer,
+    Float,
+    Boolean,
+    Datetime,
+    Array,
+    Table,
+}
+
+////////////////////////////////////////////////////////////////////
+// Value functions
+
+#[no_mangle]
+pub extern "C" fn ValueType(value: &TomlValue) -> TomlType {
+    match value {
+        &toml::Value::String(_) => TomlType::String,
+        &toml::Value::Integer(_) => TomlType::Integer,
+        &toml::Value::Float(_) => TomlType::Float,
+        &toml::Value::Boolean(_) => TomlType::Boolean,
+        &toml::Value::Datetime(_) => TomlType::Datetime,
+        &toml::Value::Array(_) => TomlType::Array,
+        &toml::Value::Table(_) => TomlType::Table,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn FreeValue(_: Option<Box<TomlValue>>) {
+    // let it drop
+}
+
+#[no_mangle]
+pub extern "C" fn CreateTable() -> Box<TomlValue> {
+    box toml::Value::Table(toml::Table::new())
+}
+
+#[no_mangle]
+pub extern "C" fn ValueTable(value: &TomlValue) -> Option<&toml::Table> {
+    match value {
+        &toml::Value::Table(ref tbl) => Some(tbl),
+        _ => None,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn ValueTableMut(value: &mut TomlValue) -> Option<&mut toml::Table> {
+    match value {
+        &mut toml::Value::Table(ref mut tbl) => Some(tbl),
+        _ => None,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn CreateArray() -> Box<TomlValue> {
+    box toml::Value::Array(toml::Array::new())
+}
+
+#[no_mangle]
+pub extern "C" fn ValueArray(value: &TomlValue) -> Option<&toml::Array> {
+    match value {
+        &toml::Value::Array(ref ary) => Some(ary),
+        _ => None,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn ValueArrayMut(value: &mut TomlValue) -> Option<&mut toml::Array> {
+    match value {
+        &mut toml::Value::Array(ref mut ary) => Some(ary),
+        _ => None,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn CreateString(input: &str) -> Box<TomlValue> {
+    box toml::Value::String(String::from(input))
+}
+
+#[no_mangle]
+pub extern "C" fn ValueString(value: &TomlValue) -> &str {
+    match value {
+        &toml::Value::String(ref string) => &string,
+        _ => "",
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn CreateInteger(i: i64) -> Box<TomlValue> {
+    box toml::Value::Integer(i)
+}
+
+#[no_mangle]
+pub extern "C" fn ValueInteger(value: &TomlValue) -> i64 {
+    match value {
+        &toml::Value::Integer(i) => i,
+        _ => 0,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn CreateFloat(f: f64) -> Box<TomlValue> {
+    box toml::Value::Float(f)
+}
+
+#[no_mangle]
+pub extern "C" fn ValueFloat(value: &TomlValue) -> f64 {
+    match value {
+        &toml::Value::Float(f) => f,
+        _ => 0.0,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn CreateBoolean(b: bool) -> Box<TomlValue> {
+    box toml::Value::Boolean(b)
+}
+
+#[no_mangle]
+pub extern "C" fn ValueBoolean(value: &TomlValue) -> bool {
+    match value {
+        &toml::Value::Boolean(b) => b,
+        _ => false,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn CreateDatetime(input: &str) -> Box<TomlValue> {
+    box toml::Value::Datetime(String::from(input))
+}
+
+#[no_mangle]
+pub extern "C" fn ValueDatetime(value: &TomlValue) -> &str {
+    match value {
+        &toml::Value::Datetime(ref dt) => &dt,
+        _ => "",
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+// Table functions
+
+#[no_mangle]
+pub extern "C" fn ParseTable(input: &str) -> Option<Box<toml::Table>> {
+    match toml::Parser::new(input).parse() {
+        Some(table) => Some(Box::new(table)),
+        None => None,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn SerializeTable(input: &toml::Table) -> Box<TomlValue> {
+    box toml::Value::String(format!("{}", toml::Value::Table(input.clone())))
+}
+
+#[no_mangle]
+pub extern "C" fn FreeTable(_: Option<Box<toml::Table>>) {
+    // implicit drop
+}
+
+#[no_mangle]
+pub extern "C" fn TableKeyCount(table: &toml::Table) -> usize {
+    table.len()
+}
+
+#[no_mangle]
+pub extern "C" fn TableKeys<'a>(table: &'a toml::Table, keys: &mut [&'a str]) {
+    for (s, t) in table.keys().zip(keys.iter_mut()) {
+        *t = s;
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn TableGet<'a, 'b>(table: &'a toml::Table, key: &'b str) -> Option<&'a TomlValue> {
+    table.get(key)
+}
+
+#[no_mangle]
+pub extern "C" fn TableGetMut<'a, 'b>(table: &'a mut toml::Table, key: &'b str) -> Option<&'a mut TomlValue> {
+    table.get_mut(key)
+}
+
+#[no_mangle]
+pub extern "C" fn TableSet(table: &mut toml::Table, key: &str, mut value: Box<TomlValue>) {
+    let inner_value = mem::replace::<TomlValue>(&mut value, toml::Value::Boolean(false));
+    table.insert(String::from(key), inner_value);
+}
+
+#[no_mangle]
+pub extern "C" fn TableRemove(table: &mut toml::Table, key: &str) {
+    table.remove(key);
+}
+
+////////////////////////////////////////////////////////////////////
+// Array functions
+
+#[no_mangle]
+pub extern "C" fn ArrayCount(array: &toml::Array) -> usize {
+    array.len()
+}
+
+#[no_mangle]
+pub extern "C" fn ArrayGet(array: &toml::Array, index: usize) -> &TomlValue {
+    &array[index]
+}
+
+#[no_mangle]
+pub extern "C" fn ArrayGetMut(array: &mut toml::Array, index: usize) -> &mut TomlValue {
+    &mut array[index]
+}
+
+#[no_mangle]
+pub extern "C" fn ArrayPush(array: &mut toml::Array, mut value: Box<TomlValue>) {
+    let inner_value = mem::replace::<TomlValue>(&mut value, toml::Value::Boolean(false));
+    array.push(inner_value);
+}
+
+#[no_mangle]
+pub extern "C" fn ArrayPop(array: &mut toml::Array) {
+    array.pop();
+}
+
